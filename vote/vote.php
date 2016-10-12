c116a2c22deacce286f83f867db8c888269730a3
Armando Gonzalez <agonz060@ucr.edu>
2016-10-12 08:51:54 -0700

    Updated files; vote.php: displays title req. error

--- vote/vote.php
+++ vote/vote.php
@@ -1,4 +1,48 @@
 <html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+
+<!-- Double click script that interacts with the list of professors displayed to user -->
+<script type="text/javascript">
+function addToSelected() {
+	// Get the name of the professor that was doubled clicked
+ 	var index = document.getElementById("profSel").selectedIndex;
+	var profName = document.getElementsByTagName("option")[index].value;
+	
+	// Check to the 'selected' list so that duplicates are not add to the list
+	var selectedProfs = document.getElementById("selected");
+	var professors = selectedProfs.options;
+	
+	var profFound = false;
+	for(var x=0; x < professors.length; x++) {
+		if(professors[x].value == profName)
+		{
+			profFound = true;
+		}
+	}
+
+	// Add the professor to the 'selected' list 	
+	if(!profFound) {
+		// Places the selected list in a variable so that options can be added to the list 
+		var option = document.createElement("option");
+		option.text = profName;
+		option.value = profName;
+		selectedProfs.add(option);
+	} else {
+		alert(profName+" is already selected to participate.");
+	}
+};
+
+// Remove the selected professor from the list
+function removeFromSelected() {
+	var selected = document.getElementById("selected");
+	selected.remove(selected.selectedIndex);			
+};
+</script>
+</head>
+
 <body>
 
 <!-- PHP begins -->
@@ -37,28 +81,81 @@
 
 ?>
 
+<!-- PHP that processes user input begins here -->
+<?php 
+	# Set voting variables
+	$title = $description = $dateAct = $dateDeact = "";
+	$errTitle = $errDesc = $errDateAct = $errDateDeact = "";
+	$validTitle = $validDateAct = $validDateDeact = false;
+
+	# User input processing begins here
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		# Check for title input; error if not provided
+		echo "In submission PHP";	
+		if(empty($_POST["title"])) {
+			echo "In title empty";
+			$errTitle = "* Title is required";
+		} else {
+			$title = cleanInput($title);
+			
+			if(empty($title)) {
+				echo "Title is empty";
+			}		
+			echo "Title: $title"; 
+			$validTitle = true;
+		}
+		
+		# Check for valid date inputs
+		if( empty($_POST["monthActive"]) || empty($_POST["dayActive"]) ) {
+			$errDateAct = "* Invalid activation date";
+		}	
+		
+		#$description = $_POST['voteDescription'];
+		#echo "$description" ;	
+		# Check for empty date inputs
+		# $emptyMonth
+		
+		#check that activation and deactivation dates were set
+	}
+
+	function cleanInput($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+?>
+
+<!-- HTML for page Voting page elements begins here --> 
 
 <!-- Display title of page -->
 <h1 align="center">Voting</h1>
 <hr> 
 
+<!-- Form input allows the user to cancel current form data, save the data, -->
+<!-- or process the data; User information remains in input area incase form -->
+<!-- data needs to be modified before being submitted -->
+<form id="votingInfo" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+
 <!-- Title of current vote -->
 <p>
-Title: <input type="text" name="title"><br>
+Title: <input type="text" name="title" value="<?php if(isset($_POST['title'])) { echo htmlentities ($_POST['title']); } ?>"/>
+<span class="error"><?php echo "$errTitle";?></span> <br>
 </p>
 
 <!-- Descriptions/Comments about vote -->
 <p>
-Description/Comments: <br><textarea name="voteDescription" rows="5" cols="70"></textarea></br>
+Description/Comments: <br><textarea name="voteDescription" form="votingInfo" rows="5" cols="70"><?php echo "$description"; ?></textarea></br>
 </p>
 
 <!-- Date vote becomes active/inactive -->
 <p>
-Date Active(MM/DD): <input size="2" name="monthActive"> / <input size="2" name="dayActive"><br><br>
-Date Deactive(MM/DD): <input size=2" type="number" name="monthDeactive"> /
-<input size="2" type="number" name="dayDeactive"><br>
+Date Active(MM/DD): <input size="2" name="monthActive" value="<?php if(isset($_POST['monthActive'])) {echo htmlentities ($_POST['monthActive']);} ?>"> / <input size="2" name="dayActive" value="<?php if(isset($_POST['dayActive'])) {echo htmlentities ($_POST['dayActive']);} ?>"><br><br>
+Date Deactive(MM/DD): <input size=2" name="monthDeactive" value="<?php if(isset($_POST['monthDeactive'])) {echo htmlentities ($_POST['monthDeactive']);} ?>"> /
+<input size="2" name="dayDeactive" value="<?php if(isset($_POST['dayDeactive'])) {echo htmlentities ($_POST['dayDeactive']);} ?>"><br>
 </p>
 
+<!-- Begin professor selection -->
 <table style="width:30%">
 <tr>
 	<td align="center">Professors</td>
@@ -96,50 +193,12 @@ Date Deactive(MM/DD): <input size=2" type="number" name="monthDeactive"> /
 </tr>
 </table>
 
-<script type="text/javascript">
-function addToSelected() {
-	// Get the name of the professor that was doubled clicked
- 	var index = document.getElementById("profSel").selectedIndex;
-	var profName = document.getElementsByTagName("option")[index].value;
-	
-	// Check to the 'selected' list so that duplicates are not add to the list
-	var selectedProfs = document.getElementById("selected");
-	var professors = selectedProfs.options;
-	
-	var profFound = false;
-	for(var x=0; x < professors.length; x++) {
-		if(professors[x].value == profName)
-		{
-			profFound = true;
-		}
-	}
-
-	// Add the professor to the 'selected' list 	
-	if(!profFound) {
-		// Places the selected list in a variable so that options can be added to the list 
-		var option = document.createElement("option");
-		option.text = profName;
-		option.value = profName;
-		selectedProfs.add(option);
-	} else {
-		alert(profName+" is already selected to participate.");
-	}
-};
-
-// Remove the selected professor from the list
-function removeFromSelected() {
-	var selected = document.getElementById("selected");
-	selected.remove(selected.selectedIndex);			
-};
-</script>
-
 <p>
 <a href="/vote/index.php "><input type="button" value="Cancel"></a>
-<form id="votingOptions" name="votingOptions">
-<input type="button" value="Save">
-<input type="button" onclick="alert('Start')" value="Start">
-</form>
+<input type="submit" value="Save">
+<input type="submit" value="Start">
 </p>
+</form>
 
 </body>
 </html>
